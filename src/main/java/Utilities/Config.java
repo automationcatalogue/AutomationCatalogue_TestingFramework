0a8335e1887fb433aa591f0090837711de3aa4c1
Laashvi <chsatish.tester@gmail.com>
2023-11-07 20:45:38 +0530

    TC03 & TC12 excel Integrations are completed

--- src/main/java/Utilities/Config.java
+++ src/main/java/Utilities/Config.java
@@ -4,6 +4,10 @@ public class Config {
     public static final String testID_HRMLogin="TC01-04";
     public static final String testID_HRMAddUser="TC02-02";
 
+    public static final String testID_HRMReports="TC03-01";
+
+    public static final String testID_DEMOApplyDiscount="TC12-01";
+
     //Common
     public static final int col_TestcaseID=0;
     public static final int col_TestcaseName=1;
@@ -21,6 +25,30 @@ public class Config {
     //HRMReports
     public static final int col_SelectReportType=4;
     public static final int col_SelectFolder=5;
+    public static final int col_ReportName=6;
+    //public static final int col_PersonalDetails=7;
+    //public static final int col_Job=8;
+    public static final int col_DisplayFields=9;
+
 
     //HRMAttendance
+
+
+
+    public static final int col_Product=4;
+    public static final int col_ApplyDiscountCode=5;
+    public static final int col_PaymentInformation=6;
+    public static final int col_SelectCreditCard=7;
+    public static final int col_CardHolderName=8;
+    public static final int col_CardNumber=9;
+    public static final int col_ExpirationDate=10;
+    public static final int col_CardCode=11;
+
+
+
+
+
+
+
 }
+
